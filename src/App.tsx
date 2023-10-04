2882acf0d15670c10b35d4275ebe1f56669ea918
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-10-04 20:17:24 -0300

    axios: consuming my json api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+interface Product {
+  id: number,
+  brand: string,
+  productName: string,
+  price: number,
+  stockQuantity: number | boolean,
+  image_url: string
+}
+
 const App = () => {
+  const [data, setData] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get<Product[]>('http://localhost:3000/products')
+      .then(response => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Erro ao buscar dados:', error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <main>
       <h1>Hello World</h1>
+      {loading ? (
+        <p>Carregando dados...</p>
+      ) : (
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>{item.brand}</li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 }

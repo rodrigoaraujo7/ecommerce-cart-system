54db109c7c95ca2b607136bb28483b7feee9b7b0
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-10-05 23:48:39 -0300

    react: calc the total price

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -6,33 +6,41 @@ import CartFooter from './Footer';
 const Cart = () => {
   const { cartItems, removeFromCart } = useCartContext();
 
+  const total = cartItems.reduce((acumulador, produto) => {
+    return acumulador + produto.price;
+  }, 0);
+
   return (
-    <div className='
-      fixed top-0 right-0 h-full w-full bg-black text-white
-      lg:w-2/5 p-12 overflow-y-auto
-    '>
-      <header>
-        <h1 className='text-right font-bold text-3xl'>Shopping cart</h1>
-      </header>
-      <ul className='list-none flex flex-col gap-8 my-8 overflow-y-auto'>
-        {cartItems.map((product, index) => (
-          <li key={index} className='flex items-center bg-grey900 rounded-lg relative'>
-            <img src={product.image_url} alt={product.productName} className='w-28' />
-            <div className='ml-4'>
-              <h2 className='font-bold'>{product.brand} <span className='font-medium'>{product.productName}</span></h2>
-              <h2 className='font-medium text-grey500 underline'>$ {product.price}</h2>
-            </div>
-            <button
-              onClick={() => removeFromCart(product)}
-              className='text-right absolute right-4'
-            >
-              <img src={deleteIcon} alt="" />
-            </button>
-          </li>
-        ))}
-      </ul>
-      <CartFooter totalPrice={2000} />
-    </div>
+    <>
+      {cartItems.length >= 1 && (
+        <div className='
+          fixed top-0 right-0 h-full w-full bg-black text-white
+          lg:w-2/5 p-12 overflow-y-auto
+        '>
+          <header>
+            <h1 className='text-right font-bold text-3xl'>Shopping cart</h1>
+          </header>
+          <ul className='list-none flex flex-col gap-8 my-8 overflow-y-auto'>
+            {cartItems.map((product, index) => (
+              <li key={index} className='flex items-center bg-grey900 rounded-lg relative'>
+                <img src={product.image_url} alt={product.productName} className='w-28' />
+                <div className='ml-4'>
+                  <h2 className='font-bold'>{product.brand} <span className='font-medium'>{product.productName}</span></h2>
+                  <h2 className='font-medium text-grey500 underline'>$ {product.price}</h2>
+                </div>
+                <button
+                  onClick={() => removeFromCart(product)}
+                  className='text-right absolute right-4'
+                >
+                  <img src={deleteIcon} alt="" />
+                </button>
+              </li>
+            ))}
+          </ul>
+          <CartFooter totalPrice={total} />
+        </div>
+      )}
+    </>
   )
 }
 
